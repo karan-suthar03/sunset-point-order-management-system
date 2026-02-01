d97729a7e13883e0a2e5d1ecb1973d21960b00e5
karan suthar <karan.suthar.lakha@gmail.com>
2026-02-02 02:48:06 +0530

    refactor: Remove delete button from Menu component

--- admin/src/pages/Menu.jsx
+++ admin/src/pages/Menu.jsx
@@ -193,13 +193,6 @@ export default function Menu() {
                                 >
                                   <Edit3 size={18} />
                                 </button>
-                                <button 
-                                  onClick={() => handleDelete(item.id)}
-                                  className="p-2 text-slate-400 hover:text-red-600 hover:bg-red-50 rounded-lg transition-colors cursor-pointer"
-                                  title="Delete"
-                                >
-                                  <Trash2 size={18} />
-                                </button>
                               </div>
                             </td>
                           </tr>
